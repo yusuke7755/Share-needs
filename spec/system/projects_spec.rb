1355985942ec899442872a2180b3def574591dee
yusuke ohta <youohta7755@gmail.com>
2021-08-28 15:10:55 +0900

    Rspecレポート作成テスト2

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -157,7 +157,7 @@ RSpec.describe "レポート作成関連", type: :system do
 
     end
 
-    it 'レポート検索：タイトル・部分一致 検索' do
+    it 'レポート検索：タイトル・部分一致 検索(ransack)' do
 
       #部署
       department1 = FactoryBot.create(:department1)
@@ -227,99 +227,161 @@ RSpec.describe "レポート作成関連", type: :system do
        expect(page).to have_content  'レポート検索'
     end
 
-    # it '顧客編集する' do
+    it 'レポート編集する' do
 
-    #   #ログインユーザー
-    #   FactoryBot.create(:employee1)
-    #   #企業
-    #   # FactoryBot.create(:customer) 
-    #   customer = FactoryBot.create(:customer)
+      #部署
+      department1 = FactoryBot.create(:department1)
+      department2 = FactoryBot.create(:department2)
+      #ログインユーザー
+      employee2 = FactoryBot.create(:employee2, department: department1)
+      employee3 = FactoryBot.create(:employee3, department: department2)
+      #会社
+      customer1 = FactoryBot.create(:customer1)
+      customer2 = FactoryBot.create(:customer2)
+      #利用者
+      customeruser1 = FactoryBot.create(:customeruser1, customer: customer1)
+      customeruser2 = FactoryBot.create(:customeruser2, customer: customer2)
+      #パッケージ
+      package = FactoryBot.create(:package1)
+      #機能
+      feature = FactoryBot.create(:feature1, package: package)
 
-    #   #利用者
-    #   FactoryBot.create(:customeruser1, customer: customer)
-    #   FactoryBot.create(:customeruser2, customer: customer)
-    #   visit root_path
-    #   click_link 'login'
-    #   fill_in 'employee_email', with: 'user1@gmail.com'
-    #   fill_in 'employee_password', with: 'password'
-    #   click_button 'login'
-    #   #ユーザー登録に移動
-    #   click_button '顧客登録'
-    #   #リストから選択
-    #   select "中川コンツェルン", from: "search_customer_id"
-    #   click_button '検索'
-    #   # 特定のUserの編集画面に移動する
-    #   all('tr td')[10].click_link 
-    #   fill_in 'customeruser_department', with: '管理部'
-    #   fill_in 'customeruser_name', with: '山田次郎'
-    #   fill_in 'customeruser_email', with: 'fghij@email.com'
-    #   fill_in 'customeruser_tel', with: '44-4444-4444'
-    #   fill_in 'customeruser_meta', with: 'Rspecテスト2'
-    #   click_on '更新'
+      #レポート
+      FactoryBot.create(:project2, customer: customer1 , customeruser: customeruser1, department: department1, employee: employee2, package: package, feature: feature )
+      FactoryBot.create(:project3, customer: customer2 , customeruser: customeruser2, department: department1, employee: employee2, package: package, feature: feature )
+      FactoryBot.create(:project4, customer: customer2 , customeruser: customeruser2, department: department2, employee: employee3, package: package, feature: feature )
 
-    # end
+      visit root_path
+      click_link 'login'
+      fill_in 'employee_email', with: 'user2@gmail.com'
+      fill_in 'employee_password', with: 'password'
+      click_button 'login'
+      #詳細ボタン確認
+      all('tr td')[6].click_link 
+      expect(page).to have_content  'レポート検索'
+      click_button '編集'
+      select '営業部', from: "project_department_id"
+      select '中', from: "project_priority"
+      fill_in 'apoint_at', with: '002021-08-25'
+      fill_in 'deadline', with: '002021-08-29'
+      fill_in 'project_description', with: 'Rspecテスト2'
+      click_button 'レポート編集'
+      expect(page).to have_content 'レポートが編集されました。'
 
-    # it '顧客削除する' do
+    end
 
-    #   #ログインユーザー
-    #   FactoryBot.create(:employee1)
-    #   #企業
-    #   # FactoryBot.create(:customer) 
-    #   customer = FactoryBot.create(:customer)
+    it 'レポート削除する' do
 
-    #   #利用者
-    #   FactoryBot.create(:customeruser1, customer: customer)
-    #   FactoryBot.create(:customeruser2, customer: customer)
-    #   visit root_path
-    #   click_link 'login'
-    #   fill_in 'employee_email', with: 'user1@gmail.com'
-    #   fill_in 'employee_password', with: 'password'
-    #   click_button 'login'
-    #   #ユーザー登録に移動
-    #   click_button '顧客登録'
-    #   #リストから選択
-    #   select "中川コンツェルン", from: "search_customer_id"
-    #   click_button '検索'
-    #   # 特定のUserの編集画面に移動する
-    #   all('tr td')[11].click_link 
-    #   # page.accept_confirm 'ユーザ情報が削除されます。よろしいですか？'
-    #   page.accept_alert
-    #    expect(page).to have_content  'ユーザを削除しました。'
+      #部署
+      department1 = FactoryBot.create(:department1)
+      department2 = FactoryBot.create(:department2)
+      #ログインユーザー
+      employee2 = FactoryBot.create(:employee2, department: department1)
+      employee3 = FactoryBot.create(:employee3, department: department2)
+      #会社
+      customer1 = FactoryBot.create(:customer1)
+      customer2 = FactoryBot.create(:customer2)
+      #利用者
+      customeruser1 = FactoryBot.create(:customeruser1, customer: customer1)
+      customeruser2 = FactoryBot.create(:customeruser2, customer: customer2)
+      #パッケージ
+      package = FactoryBot.create(:package1)
+      #機能
+      feature = FactoryBot.create(:feature1, package: package)
 
-    # end
+      #レポート
+      FactoryBot.create(:project2, customer: customer1 , customeruser: customeruser1, department: department1, employee: employee2, package: package, feature: feature )
+      FactoryBot.create(:project3, customer: customer2 , customeruser: customeruser2, department: department1, employee: employee2, package: package, feature: feature )
+      FactoryBot.create(:project4, customer: customer2 , customeruser: customeruser2, department: department2, employee: employee3, package: package, feature: feature )
 
-    # it 'レポートに登録した顧客を削除する' do
+      visit root_path
+      click_link 'login'
+      fill_in 'employee_email', with: 'user2@gmail.com'
+      fill_in 'employee_password', with: 'password'
+      click_button 'login'
+      #詳細ボタン確認
+      all('tr td')[6].click_link 
+      expect(page).to have_content  'レポート検索'
+      click_button '削除'
+      page.accept_alert
+       expect(page).to have_content  '作成したレポートが削除されました。'
 
-    #   #ログインユーザー
-    #   employee = FactoryBot.create(:employee1)
-    #   #企業
-    #   customer = FactoryBot.create(:customer)
-    #   #利用者
-    #   FactoryBot.create(:customeruser1, customer: customer)
-    #   customeruser = FactoryBot.create(:customeruser, customer: customer)
-    #   #パッケージ
-    #   package = FactoryBot.create(:package1)
-    #   #機能
-    #   feature = FactoryBot.create(:feature1, package: package)
-    #   #レポート
-    #   FactoryBot.create(:project1, customer: customer , customeruser: customeruser, employee: employee, package: package, feature: feature )
-    #   visit root_path
-    #   click_link 'login'
-    #   fill_in 'employee_email', with: 'user1@gmail.com'
-    #   fill_in 'employee_password', with: 'password'
-    #   click_button 'login'
-    #   #ユーザー登録に移動
-    #   click_button '顧客登録'
-    #   #リストから選択
-    #   select "中川コンツェルン", from: "search_customer_id"
-    #   click_button '検索'
-    #   # 特定のUserの編集画面に移動する
-    #   all('tr td')[11].click_link 
-    #   # page.accept_confirm 'ユーザ情報が削除されます。よろしいですか？'
-    #   page.accept_alert
-    #   expect(page).to have_content  'レポートに登録のあるユーザーは削除できません。'
+    end
 
-    # end
+    it '状況確認する。(お気に入り機能)' do
+
+      #部署
+      department1 = FactoryBot.create(:department1)
+      department2 = FactoryBot.create(:department2)
+      #ログインユーザー
+      employee2 = FactoryBot.create(:employee2, department: department1)
+      employee3 = FactoryBot.create(:employee3, department: department2)
+      #会社
+      customer1 = FactoryBot.create(:customer1)
+      customer2 = FactoryBot.create(:customer2)
+      #利用者
+      customeruser1 = FactoryBot.create(:customeruser1, customer: customer1)
+      customeruser2 = FactoryBot.create(:customeruser2, customer: customer2)
+      #パッケージ
+      package = FactoryBot.create(:package1)
+      #機能
+      feature = FactoryBot.create(:feature1, package: package)
+
+      #レポート
+      FactoryBot.create(:project2, customer: customer1 , customeruser: customeruser1, department: department1, employee: employee2, package: package, feature: feature )
+      FactoryBot.create(:project3, customer: customer2 , customeruser: customeruser2, department: department1, employee: employee2, package: package, feature: feature )
+      FactoryBot.create(:project4, customer: customer2 , customeruser: customeruser2, department: department2, employee: employee3, package: package, feature: feature )
+
+      visit root_path
+      click_link 'login'
+      fill_in 'employee_email', with: 'user2@gmail.com'
+      fill_in 'employee_password', with: 'password'
+      click_button 'login'
+      #詳細ボタン確認
+      all('tr td')[6].click_link 
+      expect(page).to have_content  'レポート検索'
+      click_button '状況管理する'
+      expect(page).to have_content  '状況確認を確認したい会社に設定しました。'
+
+    end
+
+    it '状況確認する。(お気に入り機能)' do
+
+      #部署
+      department1 = FactoryBot.create(:department1)
+      department2 = FactoryBot.create(:department2)
+      #ログインユーザー
+      employee2 = FactoryBot.create(:employee2, department: department1)
+      employee3 = FactoryBot.create(:employee3, department: department2)
+      #会社
+      customer1 = FactoryBot.create(:customer1)
+      customer2 = FactoryBot.create(:customer2)
+      #利用者
+      customeruser1 = FactoryBot.create(:customeruser1, customer: customer1)
+      customeruser2 = FactoryBot.create(:customeruser2, customer: customer2)
+      #パッケージ
+      package = FactoryBot.create(:package1)
+      #機能
+      feature = FactoryBot.create(:feature1, package: package)
+
+      #レポート
+      FactoryBot.create(:project2, customer: customer1 , customeruser: customeruser1, department: department1, employee: employee2, package: package, feature: feature )
+      FactoryBot.create(:project3, customer: customer2 , customeruser: customeruser2, department: department1, employee: employee2, package: package, feature: feature )
+      FactoryBot.create(:project4, customer: customer2 , customeruser: customeruser2, department: department2, employee: employee3, package: package, feature: feature )
+
+      visit root_path
+      click_link 'login'
+      fill_in 'employee_email', with: 'user2@gmail.com'
+      fill_in 'employee_password', with: 'password'
+      click_button 'login'
+      #詳細ボタン確認
+      all('tr td')[6].click_link 
+      expect(page).to have_content  'レポート検索'
+      click_button '状況管理する'
+      expect(page).to have_content  '状況確認を確認したい会社に設定しました。'
+
+    end
 
   end
+
 end
