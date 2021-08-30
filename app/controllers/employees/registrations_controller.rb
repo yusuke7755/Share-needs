7c168078d500123100a3cddd29a107ae79100d65
yusuke ohta <youohta7755@gmail.com>
2021-08-30 13:41:37 +0900

    heroku

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -15,40 +15,41 @@ class Employees::RegistrationsController < Devise::RegistrationsController
     super
   end
 
-  #GET /resource/edit
-  def edit
-    super
-  end
+  # #GET /resource/edit
+  # def edit
+  #   super
+  # end
 
-  #PUT /resource
-  def update
-    super
-  end
+  # #PUT /resource
+  # def update
+  #   super
+  # end
 
-  #DELETE /resource
-  def destroy
-    super
-  end
+  # #DELETE /resource
+  # def destroy
+  #   super
+  # end
 
   #GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
-  def cancel
-    super
-  end
+  # def cancel
+  #   super
+  # end
 
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :department_id , :admin])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :department_id , :admin])
+   
   end
 
   # The path used after sign up.
