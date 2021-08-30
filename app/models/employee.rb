e8c4e64b6e2e3420e64b27697f0492999abd79ce
yusuke ohta <youohta7755@gmail.com>
2021-08-30 22:12:17 +0900

    新規ログイン登録画面遷移修正

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,8 +8,8 @@ class Employee < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,length: { maximum: 100 },
-  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :encrypted_password, presence: true, length: { minimum: 6 }
+      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  #validates :encrypted_password, presence: true, length: { minimum: 6 }
   before_validation { email.downcase! }
   enum position: {部長: 1 , 課長: 2, 主任:3, 一般: 4 }
 
