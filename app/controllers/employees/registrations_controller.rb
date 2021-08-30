0e8793b7721d8c863122da3c521869caeeda008a
yusuke ohta <youohta7755@gmail.com>
2021-08-30 13:57:06 +0900

    heroku

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class Employees::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
 
   #GET /resource/sign_up
   def new
@@ -43,23 +42,23 @@ class Employees::RegistrationsController < Devise::RegistrationsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :department_id , :admin])
-  end
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :department_id , :admin])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :department_id , :admin])
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :department_id , :admin])
    
-  end
+  # end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    super(resource)
-  end
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 
   # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    super(resource)
-  end
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 end
