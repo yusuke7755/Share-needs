7a731d08b5ae988b2970cf5d7c9c471598a4efac
yusuke ohta <youohta7755@gmail.com>
2021-08-30 13:45:16 +0900

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :customer_managements
   root 'customer_managements#top' #追記
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
 
   devise_for :employees
     post 'employees/guest_sign_in', to: 'employees#guest_sign_in'
     post 'employees/admin_guest_sign_in', to: 'employees#admin_guest_sign_in'
 
+  resources :customer_managements
   resources :main_menus, :features ,:customers, :customerusers 
   resources :checks, only: [ :create ,:destroy]
 
